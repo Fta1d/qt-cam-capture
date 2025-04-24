6fe58b4eb16be5a82d3fa489d88aa86ae1628920
Fta1d <li4nomark228@gmail.com>
2025-04-24 13:00:50 +0300

    added controlled progress bars, imitating turret rotation

--- inc/window.h
+++ inc/window.h
@@ -5,6 +5,7 @@
 #include <QCamera>
 #include <QMediaCaptureSession>
 #include <QVideoWidget>
+#include <QProgressBar>
 
 class QPushButton;
 class Window : public QWidget
@@ -12,16 +13,31 @@ class Window : public QWidget
     Q_OBJECT
     public:
         explicit Window(QWidget *parent = 0);
+
     signals:
         void maxPressesReached();
+
     private slots:
         void slotButtonClicked(bool checked);
+
     private:
         QCamera *m_camera;
         QMediaCaptureSession *m_captureSession;
         QVideoWidget *m_videoWidget;
         QPushButton *m_button;
+        QProgressBar *x_progress_bar, *y_progress_bar;
         int m_counter;
+
+        bool left_pressed, 
+             right_pressed, 
+             up_pressed, 
+             down_pressed;
+
+    protected:
+        void setProgressBars();
+        void updateProgressBars();
+        void keyPressEvent(QKeyEvent *event);
+        void keyReleaseEvent(QKeyEvent *event);
 };
 
 #endif // WINDOW_H
--- src/window.cpp
+++ src/window.cpp
@@ -1,11 +1,16 @@
 #include "inc/window.h"
 #include <QPushButton>
 #include <QApplication>
+#include <QKeyEvent>
+#include <QTimer>
 
 Window::Window(QWidget *parent) :
 QWidget(parent) {
     setFixedSize(640, 500);
     m_counter = 0;
+
+    setFocusPolicy(Qt::StrongFocus);
+    setProgressBars();
 
     m_videoWidget = new QVideoWidget(this);
     m_videoWidget->setGeometry(0, 0, 640, 390); // x, y, width, height
@@ -21,6 +26,77 @@ QWidget(parent) {
     m_captureSession = new QMediaCaptureSession();
     m_captureSession->setCamera(m_camera);
     m_captureSession->setVideoOutput(m_videoWidget);
+
+    QTimer *timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, &Window::updateProgressBars);
+    timer->start(50);
+
+    setFocus();
+}
+
+void Window::keyPressEvent(QKeyEvent *event) {
+    if (event->key() == Qt::Key_Left) {
+        left_pressed = true;
+    } else if (event->key() == Qt::Key_Right) {
+        right_pressed = true;
+    } else if (event->key() == Qt::Key_Up) {
+        up_pressed = true;
+    } else if (event->key() == Qt::Key_Down) {
+        down_pressed = true;
+    }
+
+    QWidget::keyPressEvent(event);
+}
+
+void Window::keyReleaseEvent(QKeyEvent *event) {
+    if (event->key() == Qt::Key_Left) {
+        left_pressed = false;
+    } else if (event->key() == Qt::Key_Right) {
+        right_pressed = false;
+    } else if (event->key() == Qt::Key_Up) {
+        up_pressed = false;
+    } else if (event->key() == Qt::Key_Down) {
+        down_pressed = false;
+    }
+    
+    QWidget::keyReleaseEvent(event);
+}
+
+void Window::updateProgressBars() {
+    if (left_pressed) {
+        int x_val = x_progress_bar->value();
+        x_progress_bar->setValue(x_val - 10);
+    }
+    
+    if (right_pressed) {
+        int x_val = x_progress_bar->value();
+        x_progress_bar->setValue(x_val + 10);
+    }
+
+    if (up_pressed) {
+        int y_val = y_progress_bar->value();
+        y_progress_bar->setValue(y_val + 10);
+    }
+    
+    if (down_pressed) {
+        int y_val = y_progress_bar->value();
+        y_progress_bar->setValue(y_val - 10);
+    }
+}
+
+void Window::setProgressBars() {
+    x_progress_bar = new QProgressBar(this);
+    x_progress_bar->setFormat("%v");
+    x_progress_bar->setRange(-100, 100);
+    x_progress_bar->setValue(0);
+    x_progress_bar->setGeometry(50, 470, 200, 30);
+
+    y_progress_bar = new QProgressBar(this);
+    y_progress_bar->setFormat("%v");
+    y_progress_bar->setRange(-100, 100);
+    y_progress_bar->setValue(0);
+    y_progress_bar->setGeometry(10, 400, 30, 100);
+    y_progress_bar->setOrientation(Qt::Vertical);
 }
 
 void Window::slotButtonClicked(bool checked) {
